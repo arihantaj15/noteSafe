090f1ccd8e283430dcdba2e67d29318cc223c4da
arihantaj15 <jain.ari15@gmail.com>
2022-01-05 15:41:17 +0530

    Note-Details page

---
+++ src/app/pages/note-details/note-details.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-note-details',
+  templateUrl: './note-details.component.html',
+  styleUrls: ['./note-details.component.scss']
+})
+export class NoteDetailsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(){
+    
+  }
+  onSubmit(form: NgForm){
+       console.log(form);
+  }
+
+}
+
+
